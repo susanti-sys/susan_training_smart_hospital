16d1e012d509b4864c036fbe1488877ded98648e
susanti-sys <susanti@kodingnext.com>
2026-06-25 16:50:20 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import os
+import numpy as np
 
+st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon ="🏥", layout="wide")
+
+st.title("Smart Hospital Navigator")
